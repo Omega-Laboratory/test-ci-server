6dccbdcb31db1391db5dff8a214b677ac0ffed17
AE Tabulanga <bokor@tabulan.ga>
2020-01-29 18:17:49 +0300

    add writing version

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', function (req, res) {
-  res.send("I'm ready");
+  res.send("I'm ready\nVersion 0.0.1");
 });
 
 app.get('/game', function (req, res) {
