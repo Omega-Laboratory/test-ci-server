9f0ff0d2acea6c95b597565f7950b4def9a9ae27
AE Tabulanga <bokor@tabulan.ga>
2020-02-05 15:39:47 +0300

    change version

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', function (req, res) {
-  res.send("I'm ready\nVersion 0.1.0");
+  res.send("I'm ready\nVersion 0.1.1");
 });
 
 app.get('/game', function (req, res) {
